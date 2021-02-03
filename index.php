42326bfcc539f3ee62b4d41fe8961d1291b5720d
Florian Gubler <gubler.florian@gmx.net>
2021-02-03 14:25:48 +0000

    Inital fixes

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_index.css">
+    <script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js" data-auto-replace-svg></script>
+    <title>HomeSrv Project</title>
+</head>
+<body>
+    <div class="infobox">
+        <h1>Working on it...</h1>
+        <div class="splitline"></div>
+        <div class="icon"><i class="fas fa-globe-europe"></i></div>
+    </div>
+</body>
+</html>
